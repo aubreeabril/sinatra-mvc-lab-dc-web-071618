7eba486c60a24134b7a33e500cb66cad9d269c3b
aubreeabril <aubree.abril@gmail.com>
2018-08-21 22:21:35 -0400

    refactoring translator using regex

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,24 +1,36 @@
 class PigLatinizer
 
+  # def translate_word(text)
+  # vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  #   letters = text.split("")
+  #
+  #   if vowels.include?(letters[0])
+  #     letters << "way"
+  #     letters.join('')
+  #   else
+  #     prefix = text[0, %w(a e i o u A E I O U).map {|v| "#{text}aeiouAEIOU".index(v)}.min]
+  #       "#{text[prefix.length..-1]}#{prefix}ay"
+  #   end
+  # end
+  #
+  # def piglatinize(text)
+  #   words = text.split(" ")
+  #   words.map do |w|
+  #     translate_word(w)
+  #   end.join(' ')
+  # end
+
   def translate_word(text)
-  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    letters = text.split("")
-# binding.pry
-# puts nil
-    if vowels.include?(letters[0])
-      letters << "way"
-      letters.join('')
+    if /(^[aeiouAEIOU])/.match(text.split('')[0])
+      "#{text}way"
     else
-      prefix = text[0, %w(a e i o u A E I O U).map {|v| "#{text}aeiouAEIOU".index(v)}.min]
-        "#{text[prefix.length..-1]}#{prefix}ay"
+      parts = text.split(/([aeiou].*)/)
+      "#{parts[1]}#{parts[0]}ay"
     end
   end
 
   def piglatinize(text)
-    words = text.split(" ")
-    words.map do |w|
-      translate_word(w)
-    end.join(' ')
+    words = text.split(' ')
+    words.map {|word| translate_word(word)}.join(' ')
   end
-
 end
